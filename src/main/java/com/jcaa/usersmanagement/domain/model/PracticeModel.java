868f4bfe279ea299136a224d78f7c121d5bbf12f
Pedro Meza <pmezag@unicartagena.edu.co>
2026-05-24 18:19:22 -0500

    fix: changed practice model to be inmutable

--- src/main/java/com/jcaa/usersmanagement/domain/model/PracticeModel.java
+++ src/main/java/com/jcaa/usersmanagement/domain/model/PracticeModel.java
@@ -5,11 +5,9 @@ import com.jcaa.usersmanagement.domain.enums.DifficultyLevel;
 import com.jcaa.usersmanagement.domain.enums.PracticeType;
 import com.jcaa.usersmanagement.domain.valueobject.PracticeId;
 import com.jcaa.usersmanagement.domain.valueobject.Title;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Value;
 
-@Getter
-@AllArgsConstructor
+@Value
 public class PracticeModel {
 
     private PracticeId practiceId;
@@ -18,30 +16,28 @@ public class PracticeModel {
     private PracticeType practiceType;
 
     public static PracticeModel create(
-            String practiceId,
-            String title,
-            String difficultyLevel,
-            String practiceType
+            PracticeId practiceId,
+            Title title,
+            DifficultyLevel difficultyLevel,
+            PracticeType practiceType
     ) {
         return new PracticeModel(
-                new PracticeId(practiceId),
-                new Title(title),
-                DifficultyLevel.fromString(difficultyLevel),
-                PracticeType.fromString(practiceType)
+                practiceId,
+                title,
+                difficultyLevel,
+                practiceType
         );
     }
 
-    public void updateTitle(String title) {
-        this.title = new Title(title);
+    public PracticeModel withTitle(final Title newTitle) {
+        return new PracticeModel(practiceId, newTitle, difficultyLevel, practiceType);
     }
 
-    public void updateDifficultyLevel(String difficultyLevel) {
-        this.difficultyLevel =
-                DifficultyLevel.fromString(difficultyLevel);
+    public PracticeModel withDifficultyLevel(final DifficultyLevel newLevel) {
+        return new PracticeModel(practiceId, title, newLevel, practiceType);
     }
 
-    public void updatePracticeType(String practiceType) {
-        this.practiceType =
-                PracticeType.fromString(practiceType);
+    public PracticeModel withPracticeType(final PracticeType newType) {
+        return new PracticeModel(practiceId, title, difficultyLevel, newType);
     }
 }
